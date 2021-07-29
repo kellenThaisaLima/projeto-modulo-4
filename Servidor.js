6d68507fb0e7a3387e2e74b16c73af31b59762de
KellenThaisadeLima <kellen1393@gmail.com>
2021-07-29 13:41:36 -0300

    voltando os ajustes finais pois deram erro

--- Servidor.js
+++ Servidor.js
@@ -2,7 +2,7 @@ const Express = require('express')
 const cors = require('cors')
 const bd =require('./src/infra/sqlite-bd')
 const treinoController = require('./src/controllers/treino-controller')
-const port = process.env.PORT
+const port = 3030
 const app = Express()
 
 
