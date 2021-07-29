fef42f721434d2808227220ab2f70b172e0f9180
KellenThaisadeLima <kellen1393@gmail.com>
2021-07-29 12:26:36 -0300

    subindo ajustes finais no servidor e package.json

--- Servidor.js
+++ Servidor.js
@@ -2,7 +2,7 @@ const Express = require('express')
 const cors = require('cors')
 const bd =require('./src/infra/sqlite-bd')
 const treinoController = require('./src/controllers/treino-controller')
-const port = 3030
+const port = process.env.PORT
 const app = Express()
 
 
